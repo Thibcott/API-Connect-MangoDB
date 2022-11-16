583e471cf085aa7761df75e9e2e60579716cc6ee
Thibcott <thibcott@outlook.com>
2022-11-16 21:55:10 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Add from './components/Add.vue'
+import Epreuves from './components/Epreuves.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Bulletins de notes 📊</h1>
+  </header>
+  <main>
+    <Add />
+    <Epreuves />
+  </main>
+</template>
+
+<style scoped>
+header {
+  color:white;
+  text-align: center;
+  margin:30px;
+}
+</style>
